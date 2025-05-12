da17a57fbd9174178bbf844351b6967479c05da0
marcus <marcus6103@gmail.com>
2025-05-12 15:02:21 +0200

    html

--- src/main/java/Presentation/HomeController.java
+++ src/main/java/Presentation/HomeController.java
@@ -1,10 +1,17 @@
 package Presentation;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HomeController {
     @RequestMapping
     public String home() {
         return "Home";
+    }
+    @GetMapping("/sell")
+    public String profile() {
+        return "Profile";
     }
 }
