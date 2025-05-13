f05620031634aa135eb8ee4245e9de903dbf71d6
marcus <marcus6103@gmail.com>
2025-05-13 21:51:17 +0200

    update

---
+++ src/main/java/Infrastructure/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package Infrastructure;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Rhselev6687");
+        return dataSource;
+    }
+
+}
